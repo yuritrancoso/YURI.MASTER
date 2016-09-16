bb5819e1911915d872a3cad9d18b6308d2bc6460
Yuri Trancoso <yuri.trancoso@escolarmanager.com.br>
2016-09-15 21:24:51 -0300

    Add files via upload

---
+++ C#_YURI_TESTE_AES.CRIPTO_HASH.CODE/CriptografiaDados/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CriptografiaDados
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMD5_Click(object sender, RoutedEventArgs e)
+        {
+            txtResposta.Text =  Helpers.CriptografiaHash.GetMD5Hash(txtSemCript.Text);
+        }
+
+        private void buttonSHA1_Click(object sender, RoutedEventArgs e)
+        {
+            txtResposta.Text = Helpers.CriptografiaHash.GerarSHA1_2(txtSemCript.Text);
+        }
+
+        private void buttonCriptSim_Click(object sender, RoutedEventArgs e)
+        {
+            txtCript.Text = Helpers.CriptografiaSimetrica.Encrypt(txtSemCript.Text);
+        }
+
+        private void buttonDecriptSim_Click(object sender, RoutedEventArgs e)
+        {
+            txtSemCript.Text = Helpers.CriptografiaSimetrica.Decrypt(txtCript.Text);
+        }
+
+        
+        }
+    }
+}
